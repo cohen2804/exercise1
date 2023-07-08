46896564608ab733a639d001b8a31a0f6695804d
Ran Cohen <ran@softsolutions.co.il>
2023-07-08 16:57:21 +0300

    second commit - tag v1.0

--- main.go
+++ main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello from exercise 1")
+
+	fmt.Println("Hello from exercise TAG 1")
+}
+
+func newFunc() {
+	fmt.Println("Hello from newFunc of exercise 1")
 }
