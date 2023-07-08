5eb311e965438764e48e1bd12a579933346094b0
Ran Cohen <ran@softsolutions.co.il>
2023-07-08 18:03:58 +0300

    Make functions as public

--- exeTest.go
+++ exeTest.go
@@ -2,14 +2,14 @@ package exeTest
 
 import "fmt"
 
-func exe1() {
+func Exe1() {
 	fmt.Println("Hello from exe1 of exercise 1")
 }
 
-func exe2() {
+func Exe2() {
 	fmt.Println("Hello from exe2 of exercise 1")
 }
 
-func exe3() {
+func Exe3() {
 	fmt.Println("Hello from exe3 of exercise 1")
 }
